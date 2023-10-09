b9ef78da5f67628dffb84446f297fc82a8e1be0d
geracomputer <gerardomercadohurtado@gmail.com>
2023-10-09 10:44:56 -0600

    comit done by Gerardo Mercado To set changes in first window

--- app/src/main/java/com/example/blocknotas2023/Ventana1.kt
+++ app/src/main/java/com/example/blocknotas2023/Ventana1.kt
@@ -1,2 +1,145 @@
 package com.example.blocknotas2023
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.blocknotas2023.ui.theme.BlockNotas2023Theme
+
+class Ventana1 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BlockNotas2023Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.onPrimary
+                ) {
+                        Greeting("Android")
+                        ListaPrincipal()
+                }
+            }
+        }
+    }
+}
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListaPrincipal() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            BarraDeBusqueda()
+        }
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = colorResource(id = R.color.orange700)
+        ) {
+            Spacer(modifier = Modifier.height(10.dp))
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxHeight()
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(600.dp))
+                FloatingActionButton(
+                    onClick = { },
+                    modifier = Modifier.size(100.dp).padding(10.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.campana),
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun BarraDeBusqueda() {
+    var textoBusqueda by remember { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    SelectionContainer {
+        BasicTextField(
+            value = textoBusqueda,
+            onValueChange = { newValue ->
+                textoBusqueda = newValue
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    keyboardController?.hide()
+                }
+            ),
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+    }
+    IconButton(
+        onClick = {
+            keyboardController?.hide()
+        },
+        modifier = Modifier.padding(1.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Search,
+            contentDescription = null,
+            tint = Color.Black
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun visualization1() {
+    BlockNotas2023Theme {
+        ListaPrincipal()
+    }
+}
+
