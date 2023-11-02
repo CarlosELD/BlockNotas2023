08858fff78d991f912f06dfe31a1004b5b5720fd
CarlosELD <89476876+CarlosELD@users.noreply.github.com>
2023-11-02 11:01:18 -0600

    Se implemento algunos ajustes para la implementación de un diseño adaptable para las interfaces Ventana3.kt y Ventana4.kt.

--- app/src/main/java/com/example/blocknotas2023/Ventana3.kt
+++ app/src/main/java/com/example/blocknotas2023/Ventana3.kt
@@ -1,97 +1,85 @@
 package com.example.blocknotas2023
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.blocknotas2023.ui.theme.BlockNotas2023Theme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Controles() {
-    var text by remember { mutableStateOf("") }
-    var value by remember { mutableStateOf("") }
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = colorResource(id = R.color.orange700)
     ) {
-        Column (
+        Column(
             modifier = Modifier
-                .padding(20.dp)
-                .fillMaxHeight().fillMaxWidth(),
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(20.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            FloatingActionButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .size(110.dp)
-                    .padding(10.dp)
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
             ) {
-                   Image(
-                       painter = painterResource(id = R.drawable.historial),
-                       contentDescription = null
-                   )
+                FloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .size(110.dp)
+                        .padding(10.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.historial),
+                        contentDescription = null
+                    )
+                }
+                FloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .size(110.dp)
+                        .padding(10.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.videos),
+                        contentDescription = null
+                    )
+                }
             }
-            FloatingActionButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .size(110.dp)
-                    .padding(10.dp)
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
             ) {
-                   Image(
-                       painter = painterResource(id = R.drawable.videos),
-                       contentDescription = null
-                   )
-            }
-            FloatingActionButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .size(110.dp)
-                    .padding(10.dp)
-            ) {
-                   Image(
-                       painter = painterResource(id = R.drawable.fotos),
-                       contentDescription = null
-                   )
-            }
-            FloatingActionButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .size(110.dp)
-                    .padding(10.dp)
-            ) {
-                   Image(
-                       painter = painterResource(id = R.drawable.musica),
-                       contentDescription = null
-                   )
+                FloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .size(110.dp)
+                        .padding(10.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.fotos),
+                        contentDescription = null
+                    )
+                }
+                FloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .size(110.dp)
+                        .padding(10.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.musica),
+                        contentDescription = null
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/example/blocknotas2023/Ventana4.kt
+++ app/src/main/java/com/example/blocknotas2023/Ventana4.kt
@@ -4,22 +4,11 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -34,8 +23,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.blocknotas2023.ui.theme.BlockNotas2023Theme
@@ -49,13 +36,13 @@ class Ventana4 : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.onPrimary
                 ) {
-                    /*Greeting("Android")*/
                     Videos()
                 }
             }
         }
     }
 }
+
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -71,7 +58,6 @@ fun Videos() {
             Column(
                 modifier = Modifier
                     .padding(16.dp)
-                    .fillMaxHeight()
                     .fillMaxSize(),
                 verticalArrangement = Arrangement.spacedBy(20.dp)
             ) {
@@ -79,7 +65,7 @@ fun Videos() {
                 TextField(
                     label = { Text(text = "Descripción") },
                     value = value,
-                    modifier = Modifier.width(400.dp),
+                    modifier = Modifier.fillMaxWidth(),
                     onValueChange = { value = it },
                     maxLines = 5
                 )
@@ -89,19 +75,17 @@ fun Videos() {
                     horizontalArrangement = Arrangement.Center
                 ) {
                     Column(
-                        modifier = Modifier.fillMaxWidth(),
                         horizontalAlignment = Alignment.CenterHorizontally
-                    )
-                    {
+                    ) {
                         Button(
-                            onClick = { },
+                            onClick = { /*TODO*/ },
                             modifier = Modifier.padding(20.dp),
                             colors = ButtonDefaults.buttonColors(Color.Red)
-                            ) {
+                        ) {
                             Text(text = "Tomar foto")
-                            }
+                        }
                         Button(
-                            onClick = { },
+                            onClick = { /*TODO*/ },
                             modifier = Modifier.padding(20.dp),
                             colors = ButtonDefaults.buttonColors(Color.Red)
                         ) {
@@ -121,3 +105,4 @@ fun visualization4() {
         Videos()
     }
 }
+
